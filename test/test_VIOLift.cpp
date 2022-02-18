10e29832d61474399db94dd1bf56c5ad5cfe7f5f
Pieter van Goor <pieter.vangoor@anu.edu.au>
2022-02-18 17:30:51 +1100

    Add license

--- test/test_VIOLift.cpp
+++ test/test_VIOLift.cpp
@@ -1,18 +1,18 @@
 /*
-    This file is part of GIFT.
+    This file is part of EqVIO.
 
-    GIFT is free software: you can redistribute it and/or modify
+    EqVIO is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    GIFT is distributed in the hope that it will be useful,
+    EqVIO is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with GIFT.  If not, see <http://www.gnu.org/licenses/>.
+    along with EqVIO.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "eigen3/Eigen/Dense"
