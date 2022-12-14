1e17cdfe0735d24966a1706dafc0bd19f7f33067
Pieter van Goor <pietervangoor@anu.edu.au>
2022-12-15 10:40:10 +1100

    remove other external and add as submodules

--- external/argparse/tools/build.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DARGPARSE_BUILD_TESTS=ON
-ninja -C build
--- external/argparse/tools/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DARGPARSE_BUILD_TESTS=ON
-ninja -C build
